dac7bed13b5da18cf1aaf3dad9956527544b286d
DhanaCorredor <corredorsadhana@gmail.com>
2026-04-09 16:31:32 +0200

    Refactor: Nueva estructura de carpetas y archivos

---
+++ features/data/levels.js
@@ -0,0 +1,6 @@
+// Configuración de dificultades
+
+export const levels = [
+  { level: 1, speed: 1 },
+  { level: 2, speed: 1.5 },
+];
---
+++ main.js
@@ -0,0 +1,7 @@
+// Punto de entrada principal del juego
+
+import { startGame } from './features/core/game.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  startGame();
+});
