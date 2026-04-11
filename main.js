20d60bd41f16d4ed96ac068046c3867f2264bc77
jass2501 <jasmineagudomamani@gmail.com>
2026-04-11 13:10:43 +0200

    update main

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // Punto de entrada principal del juego
 
-import { startGame } from './features/core/game.js';
+/*import { startGame } from './features/core/game.js';*/
+import { Views } from "./views.js";
 
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
   startGame();
-});
+});*/
+
+const root = document.querySelector("main");
+const view = new Views(root);
+
+view.showLoading();
+
+console.log(root);
