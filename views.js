048958c3868025c50207eb48541a79f7ba736f90
jass2501 <jasmineagudomamani@gmail.com>
2026-04-11 13:11:10 +0200

    update views

--- views.js
+++ views.js
@@ -1,8 +1,24 @@
 // Actualiza el HUD (puntos/reloj) en el DOM
 
-export function updateHUD(score, time) {
+/*export function updateHUD(score, time) {
   const scoreEl = document.querySelector('#score');
   const timeEl = document.querySelector('#time');
   if (scoreEl) scoreEl.textContent = score;
   if (timeEl) timeEl.textContent = time;
 }
+*/
+import { getBestScores } from "./features/core/storage.js";
+
+export class Views {
+  constructor(root) {
+    this.root = root;
+    this.currentGame = null;
+  }
+  showLoading() {
+    this.root.innerHTML = `
+      <div class="progress">
+        Estamos trabajando en ello, en progreso de creación...
+      </div>
+    `;
+  }
+};
